174243afb2f0882ff1dabf434611d11a46a8d6b9
Mostafa Amin <125675410+moustafaamindev@users.noreply.github.com>
2023-10-12 17:36:49 +0300

    add section feature and slider

---
+++ src/app/views/home/components/slider/slider.component.ts
@@ -0,0 +1,39 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Component, OnInit } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-slider',
+  templateUrl: './slider.component.html',
+  styleUrls: ['./slider.component.scss'],
+})
+export class SliderComponent {
+  public slider: any;
+  customOptions: OwlOptions = {
+    loop: true,
+    margin: 0,
+    dots: true,
+    animateOut: 'fadeOut',
+    autoplay: true,
+    autoplayTimeout: 5000,
+    autoplayHoverPause: true,
+    responsive: {
+      0: {
+        items: 1,
+      },
+    },
+    nav: false,
+  };
+
+  constructor(private http: HttpClient) {}
+
+  createslider() {
+    this.http.get('assets/api/home.json').subscribe((data: any) => {
+      this.slider = data.slider;
+    });
+  }
+  ngOnInit() {
+    this.createslider();
+  }
+}
